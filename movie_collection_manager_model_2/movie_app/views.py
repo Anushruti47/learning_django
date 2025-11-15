d2f8a061834bfa69cdcd39e55c7dd7f2583f58ce
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2025-11-15 12:07:01 +0530

    updated

--- movie_collection_manager_model_2/movie_app/views.py
+++ movie_collection_manager_model_2/movie_app/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+# Method 2: Using save() method
+def add_movie(request):
+    movie=Movie(
+        title="Inception",
+        director="Christopher Nolan",
+        release_year=2010,
+        imdb_id="tt1375666"
+    )
+    movie.save()
+    all_movies=Movie.objects.all()
+    context={
+        'new_movie':movie,
+        'all_movies':all_movies
+    }
+    return render(request,'movies.html',context)
+
+# Method 1: Using create() method
+# def add_movie(request):
+
+#     movie = Movie.objects.create(
+#         title="Inception",
+#         director="Christopher Nolan",
+#         release_year=2010,
+#         imdb_id="tt1375666",
+#     )
+
+#     all_movies = Movie.objects.all()
+
+#     context = {
+#         'new_movie': movie,
+#         'all_movies': all_movies
+#     }
+
+#     return render(request, 'movies.html', context)
