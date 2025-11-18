884d5b00a9cd3314a8123b96d2a21736ff998f30
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2025-11-18 12:04:26 +0530

    updated

--- delete_completedTasks_toDoList/toDo_app/views.py
+++ delete_completedTasks_toDoList/toDo_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Tasks
 
 # Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+def tasks(request):
+    if not Tasks.objects.exists():
+        Tasks.objects.create(title="Buy groceries", completed=True)
+        Tasks.objects.create(title="Do laundry", completed=False)
+        Tasks.objects.create(title="Clean room", completed=True)
+        Tasks.objects.create(title="Write report", completed=False)
+
+    tasks=Tasks.objects.all()
+
+    return render(request,"tasks.html",{'tasks':tasks})
+
+def delete_completed_tasks(request):
+    
+    Tasks.objects.filter(completed=True).delete()
+
+    tasks=Tasks.objects.all()
+    
+    return render(request,"tasks.html",{'tasks':tasks})
