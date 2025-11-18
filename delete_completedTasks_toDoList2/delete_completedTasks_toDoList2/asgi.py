047a6e4818df0774aed8b2ad910e4a86be7f4350
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2025-11-18 18:44:27 +0530

    Initialized

---
+++ delete_completedTasks_toDoList2/delete_completedTasks_toDoList2/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for delete_completedTasks_toDoList2 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "delete_completedTasks_toDoList2.settings"
+)
+
+application = get_asgi_application()
