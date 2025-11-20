3f52f062d964f1494e209e1a37a2c9325568ef0f
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2025-11-20 14:58:57 +0530

    updated

--- book_catalog_withValidations/book_app/models.py
+++ book_catalog_withValidations/book_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Book(models.Model):
+    title=models.CharField(max_length=200,blank=False)
+    author=models.CharField(max_length=100)
+    publication_year=models.IntegerField(validators=[MinValueValidator(1800),MaxValueValidator(2023)])
+    isbn=models.CharField(max_length=13,unique=True)
+    genre=models.CharField(max_length=50,choices=[
+        ('FIC','Fiction'),
+        ('NON','Non-Fiction'),
+        ('SCI','Science'),
+        ('HIS','History')
+    ])
+    website=models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
