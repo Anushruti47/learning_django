518a8910ecb173fa7630f15a2e8234e365e2b1d3
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2025-11-24 13:34:23 +0530

    updated

--- many2one_library_management_system/library/views.py
+++ many2one_library_management_system/library/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Author,Book
+from datetime import date
 
 # Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+def book_list(request):
+    if not Author.objects.exists():
+        author1=Author.objects.create(name="George Orwell", birth_date=date(1903, 6, 25))
+        author2 = Author.objects.create(name="J.K. Rowling", birth_date=date(1965, 7, 31))
+
+        Book.objects.create(title="1984", author=author1, publication_date=date(1949, 6, 8), isbn="1234567890123")
+        Book.objects.create(title="Animal Farm", author=author1, publication_date=date(1945, 8, 17), isbn="1234567890124")
+        Book.objects.create(title="Harry Potter and the Philosopher's Stone", author=author2, publication_date=date(1997, 6, 26), isbn="1234567890125")
+
+    books=Book.objects.select_related("author").all()
+    return render(request,"books.html",{"books":books})
--- many2one_library_management_system/many2one_library_management_system/urls.py
+++ many2one_library_management_system/many2one_library_management_system/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from library import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("",views.home,name="home"),
+    path("home/",views.home,name="home"),
+    path("books/",views.book_list,name="book_list"),
+    # path("admin/", admin.site.urls),
 ]
