7bfdf8e9e20dfa724cda6636dc57e49a6bdab318
Anushruti47 <240118848+Anushruti47@users.noreply.github.com>
2025-11-30 13:30:37 +0530

    updated

--- library_cardSystem/library/models.py
+++ library_cardSystem/library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Patron(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+
+    def __str__(self):
+        return self.name
+    
+class LibraryCard(models.Model):
+    patron=models.OneToOneField(Patron,on_delete=models.CASCADE)
+    card_number=models.CharField(max_length=10,unique=True)
+    issue_date=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Card {self.card_number} - {self.patron.name}"
