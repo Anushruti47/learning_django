70934aec2b9f4e680bf4f4043a72231488c02ccd
Anushruti47 <240118848+Anushruti47@users.noreply.github.com>
2025-11-30 14:04:50 +0530

    updated

--- library_cardSystem/library/views.py
+++ library_cardSystem/library/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import Patron, LibraryCard
 
 # Create your views here.
 def home(request):
+    Patron.objects.all().delete()
+    LibraryCard.objects.all().delete()
 
-    return render(request,"home.html")
+    # Create Patrons
+    p1=Patron.objects.create(name="Alice",email="alice@example.com")
+    p2=Patron.objects.create(name="Bob",email="bob@example.com")
+
+    # Create Library Cards
+    LibraryCard.objects.create(patron=p1,card_number="LC1001")
+    LibraryCard.objects.create(patron=p2,card_number="LC1002")
+
+    patrons=Patron.objects.all()
+
+    return render(request,"home.html",{'patrons':patrons})
