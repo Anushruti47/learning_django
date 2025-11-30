12341d26408d8765703d0b46f66fb5f3a63a68be
Anushruti47 <240118848+Anushruti47@users.noreply.github.com>
2025-11-30 11:33:10 +0530

    updated

--- studentCourse_registration/college/models.py
+++ studentCourse_registration/college/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class Course(models.Model):
+    name=models.CharField(max_length=200)
+    students=models.ManyToManyField(Student, related_name='courses')
+
+    def __str__(self):
+        return self.name
--- studentCourse_registration/college/views.py
+++ studentCourse_registration/college/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from .models import Student, Course
 
-# Create your views here.
+def home(request):
+    # Delete previous data
+    Student.objects.all().delete()
+    Course.objects.all().delete()
+
+    # Create students
+    alice=Student.objects.create(name="Alice")
+    bob=Student.objects.create(name="Bob")
+    carol=Student.objects.create(name="Carol")
+
+    # Create courses
+    python=Course.objects.create(name="Python")
+    web=Course.objects.create(name="Web")
+    django=Course.objects.create(name="Django")
+
+    # Enroll Alice and Bob to python
+    python.students.add(alice,bob)
+
+    # Enroll Alice in Web
+    web.students.add(alice)
+
+    # Enroll carol in django
+    django.students.add(carol,alice,bob)
+
+    # Remove bob from python 
+    python.students.remove(bob)
+
+    # Clear students from web
+    web.students.clear()
+
+    # Enroll only carol in web
+    web.students.set([carol])
+    web.students.remove(carol)
+
+    # Access alll students and courses
+    students=Student.objects.all()
+    courses=Course.objects.all()
+
+    context={
+        'students':students,
+        'courses':courses
+    }
+    return render(request,"home.html",context)
+
+
