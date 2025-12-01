346c3271f0d309b0909a7f0f1aad48f7343037d7
Anushruti47 <240118848+Anushruti47@users.noreply.github.com>
2025-12-02 00:26:03 +0530

    updated

--- productReview_system/product_review/views.py
+++ productReview_system/product_review/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product, Review
+from datetime import datetime
 
-# Create your views here.
+def home(request):
+    # Sample Data
+    if not Product.objects.exists():
+        p1 = Product.objects.create(name="Laptop", description="Gaming laptop", price=1500.0, in_stock=True)
+        p2 = Product.objects.create(name="Headphones", price=200.0)
+        
+        Review.objects.create(product=p1, rating=5, comment="Excellent performance")
+        Review.objects.create(product=p1, rating=4)
+        Review.objects.create(product=p2, rating=3, comment="Decent sound quality")
+
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+
