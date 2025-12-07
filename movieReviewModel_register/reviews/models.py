5a9bd198a59533496d4c17654b2f6363a968317e
Anushruti47 <240118848+Anushruti47@users.noreply.github.com>
2025-12-08 00:41:41 +0530

    updated

--- movieReviewModel_register/reviews/models.py
+++ movieReviewModel_register/reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_title=models.CharField(max_length=200)
+    director=models.CharField(max_length=100)
+    release_year=models.IntegerField()
+    rating=models.FloatField()
+    review_text=models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.movie_title
