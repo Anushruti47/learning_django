1f1983b7bbf586fd4623798aae0022b4baa7ff04
Anushruti47 <240118848+Anushruti47@users.noreply.github.com>
2025-12-18 13:38:55 +0530

    updated

---
+++ dailyMoodTracker_form/moodtrackerapp/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class MoodTrackerForm(forms.Form):
+    pass
--- dailyMoodTracker_form/moodtrackerapp/views.py
+++ dailyMoodTracker_form/moodtrackerapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import MoodTrackerForm
 
 # Create your views here.
+def track_mood(request):
+    form=MoodTrackerForm()
+
+    context={
+        'form_title':"Mood Tracker App",
+        'form':form
+    }
+    return render(request,"moodtracker.html",context)
