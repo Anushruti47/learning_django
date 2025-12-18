28aa6cc85cc3df4b7d3c3b6acf4ff6062c05bc3c
Anushruti47 <240118848+Anushruti47@users.noreply.github.com>
2025-12-18 12:29:43 +0530

    updated

---
+++ communitySuggestion_boxFormSetup/feedback/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class SuggestionForm(forms.Form):
+    pass
--- communitySuggestion_boxFormSetup/feedback/views.py
+++ communitySuggestion_boxFormSetup/feedback/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import SuggestionForm
+from django.views import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def suggestion_form_view(request):
+    form=SuggestionForm()
+    context={
+        'form_title':'Community Suggestion Box',
+        'form':form
+    }
