184bd7f6002411372aae17521b7a7722b13c684a
Anushruti47 <240118848+Anushruti47@users.noreply.github.com>
2025-12-25 13:06:25 +0530

    updated

---
+++ appointment_request_formFields/clinicapp/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class AppointmentForm(forms.form):
+    full_name=forms.CharField(label="Patient's Full Name",max_length=100,required=True)
+    appointment_date=forms.DateField(label="Preferred Appointment Date",required=True)
+    appointment_time=forms.TimeField(label="Preferred Appointment Time",required=True)
+    reason=forms.CharField(label="Reason for Visit",required=True)
+    phone_number=forms.CharField(label="Contact Form Number",required=False)
+    email=forms.EmailField(label="Your Email Field",required=True)
