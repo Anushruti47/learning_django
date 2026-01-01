ee3059294e858b3f061e0358c7771c18cdaf8dde
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2026-01-02 01:39:47 +0530

    updated

--- workshopCreation_FormValidation/workshop_app/templates/forms.py
+++ workshopCreation_FormValidation/workshop_app/templates/forms.py
@@ -1,3 +1,7 @@
 from django import forms
 
-class 
+class WorkshpForm(forms.Form):
+    title=forms.CharField(max_length=200)
+    max_participants=forms.IntegerField()
+    registration_deadline=forms.DateField()
+    workshop_date=forms.DateField()
