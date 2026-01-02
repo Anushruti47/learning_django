d81e5fd06a2259d5dc27d548c17d83a0ff4166b9
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2026-01-02 17:08:52 +0530

    updated

---
+++ productFeedback_formValidation/productFeedback_formValidation/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for productFeedback_formValidation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "productFeedback_formValidation.settings"
+)
+
+application = get_asgi_application()
